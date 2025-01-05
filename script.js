2f4cd7709c9f05fc581e71f391729a64ab667706
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-05 07:31:42 +0400

    complete creating loop for playing the game to the end

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 (function() {
 
 const createBoard = () => {
-	return { "a": ["O", "X", "O"],
-			 "b": ["O", "X", ""],
-			 "c": ["O", "", "O"] }
+	return { "a": ["", "", ""],
+			 "b": ["", "", ""],
+			 "c": ["", "", ""] }
 }
 
 function checkGame(board, user){
@@ -17,45 +17,57 @@ function checkGame(board, user){
 		if (	board[Object.keys(board)[i]][0].length == 1 && 
 				board[Object.keys(board)[i]][0] == board[Object.keys(board)[i]][1] && 
 				board[Object.keys(board)[i]][1] == board[Object.keys(board)[i]][2]){
-			console.log(board)
-			console.log("Game Over");
+			return "Game Over" ;
 		} else if (	board["a"][i].length == 1 && 
 				board["a"][i] == board["b"][i] &&
 				board["b"][i] == board["c"][i]){
-			console.log(board)
-			console.log("Game Over");
+			return "Game Over";
 		}
 	}
 	if (board["a"][0].length == 1 && 
 		board["a"][0] == board["b"][1] && 
 		board["b"][1] == board["c"][2]) {
-		console.log(board)
-		console.log("Game Over");
+		return "Game Over";
 	}
 
 	if (board["a"][2].length == 1 && 
 		board["a"][2] == board["b"][1] && 
 		board["b"][1] == board["c"][0]) {
-		console.log(board)
-		console.log("Game Over");
+		return "Game Over";
 	}
 }
 
 function newGame() {
 	let newPlayer = createBoard();
 	let user1 = "X"/*window.prompt("X or O");*/
-	let user2;
+	let user2, tt;
 	if (user1 == "X") {
 		user2 = "O";
 	} else {
 		user2 = "X";
 	}
-	
 
-	checkGame(newPlayer, user1);
+	function playTurn(place, user) {
+		let placeArr = place.split(" ");
+		newPlayer[placeArr[0]][Number(placeArr[1])] = user;
+	}
 
+	let n = 0
+	while (true) {
+		playTurn(window.prompt("user1 turn"), user1);
+		tt = checkGame(newPlayer, user1);
+		if (tt != undefined) {
+			console.log(tt);
+			return false;
+		}
 	
-			
+		playTurn(window.prompt("user2 turn"), user2);
+		tt = checkGame(newPlayer, user2);
+		if (tt != undefined) {
+			console.log(tt);
+			return false;
+		}
+	}
 }
 
 newGame();
