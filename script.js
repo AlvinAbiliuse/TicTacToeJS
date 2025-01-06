c5426db935e7bb71a7fd6118f53ccb1af105dc2c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-06 06:17:31 +0400

    progress on ttt ui

--- script.js
+++ script.js
@@ -11,29 +11,29 @@ function checkGame(board, user){
 	for (i in ["a", "b", "c"]) {
 		if (board[Object.keys(board)[i]].includes("")) checkN++;
 	}
-	if (checkN == 0) console.log("It's a Tie!");
+	if (checkN == 0) endGame("It's a Tie!", currentPlayer);
 	
 	for (i in ["a", "b", "c"]) {
 		if (	board[Object.keys(board)[i]][0].length == 1 && 
 				board[Object.keys(board)[i]][0] == board[Object.keys(board)[i]][1] && 
 				board[Object.keys(board)[i]][1] == board[Object.keys(board)[i]][2]){
-			return "Game Over" ;
+			endGame("Game Over", currentPlayer) ;
 		} else if (	board["a"][i].length == 1 && 
 				board["a"][i] == board["b"][i] &&
 				board["b"][i] == board["c"][i]){
-			return "Game Over";
+			endGame("Game Over", currentPlayer);
 		}
 	}
 	if (board["a"][0].length == 1 && 
 		board["a"][0] == board["b"][1] && 
 		board["b"][1] == board["c"][2]) {
-		return "Game Over";
+		endGame("Game Over", currentPlayer);
 	}
 
 	if (board["a"][2].length == 1 && 
 		board["a"][2] == board["b"][1] && 
 		board["b"][1] == board["c"][0]) {
-		return "Game Over";
+		endGame("Game Over", currentPlayer);
 	}
 }
 
@@ -57,9 +57,16 @@ function updateDOM() {
 
 }
 
-function playTurn(place, user) {
+function playTurn(place, user, e) {
 	let placeArr = place.split("");
 	newPlayer[placeArr[0]][Number(placeArr[1])] = user;
+	if (currentPlayer == "player1") {
+		console.log(e.target.id);
+		e.target.textContent = user;
+	} else {
+		console.log(e.target.id);
+		e.target.textContent = user;
+	}
 }
 
 /*
@@ -93,13 +100,27 @@ function playTurn(place, user) {
 }
 
 */
-
-let currentPlayer = "user1";
+function endGame(result, user) {
+	console.log(result);
+	console.log(user);
+	if (result == "Game Over" && user == "player1") {
+		result.textContent = "Player 1 Wins!";
+	} else if (result == "Game Over" && user == "player2") {
+		result.textContent = "Player 2 Wins!";
+	} else {
+		result.textContent = result;
+	}
+	currentPlayer = "";
+}		
+	
+let currentPlayer = "";
 let newPlayer, user1, user2, result;
 
 const newGameButton = document.querySelector(".newGame");
 const modal = document.querySelector("dialog");
 const selectButton = document.querySelector(".choice");
+const msgEl = document.querySelector(".result");
+
 
 newGameButton.addEventListener("click", () => modal.showModal());
 
@@ -107,11 +128,36 @@ selectButton.addEventListener("click", (e) => {
 			if (e.target.textContent == "x") {
 				modal.close();
 				newGame("x");
+				msgEl.textContent = "Player 1 turn"
 			} else if (e.target.textContent == "o") {
 				modal.close();
 				newGame("o");
+				msgEl.textContent = "Player 1 turn"
 			}
+			currentPlayer = "player1";
 });
+
+const gameBoard = document.querySelector(".board");
+gameBoard.addEventListener("click", (e) => {
+	if (currentPlayer.length > 1 && 
+		e.target.textContent.length == 0) {
+			if (msgEl.textContent == "Player 1 turn") {
+				msgEl.textContent = "Player 2 turn";
+			} else {
+				msgEl.textContent = "Player 1 turn";
+			}	
+			if (currentPlayer == "player1") {
+				playTurn(e.target.id, user1, e);
+				checkGame(newPlayer, user1);
+				currentPlayer = "player2";
+			} else if (currentPlayer == "player2") {
+				playTurn(e.target.id, user2, e);
+				checkGame(newPlayer, user2);
+				currentPlayer = "player1";
+			}
+		}
+	});
+		
 			
 
 
